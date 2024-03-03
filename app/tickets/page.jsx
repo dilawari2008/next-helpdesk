3113ccf3096958c24fa0dfc5db3a4bcc8c5a8a97
dilawari2008 <dilawari2008@gmail.com>
2024-03-03 13:32:36 +0530

    Create Ticket BUtton

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,15 +1,22 @@
 import React, { Suspense } from 'react'
 import TicketList from './TicketList'
 import Loading from '../loading'
+import Link from 'next/link'
 
 export default function page() {
   return (
     <main>
       <nav>
-        <div>
-          <h2>Tickets</h2>
-          <p><small>Currently open tickets.</small></p>
+        <div className='flex w-full h-full'>
+          <div>
+            <h2>Tickets</h2>
+            <p><small>Currently open tickets.</small></p>
+          </div>
+          <button className="btn bg-primary border-black border-2 rounded-md ml-10">
+            <Link href="/tickets/create" className='text-white'>Create Ticket</Link>
+          </button>
         </div>
+        
       </nav>
       <Suspense fallback={<Loading/>}>
         <TicketList/>
